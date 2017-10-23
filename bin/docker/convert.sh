255cbd52e8843b2be8bc584e80d31310bc4746b9
Nicolas Hefti <nicolas@hefti.fr>
2017-10-23 19:54:36 +0200

    Update with image from Docker Hub

--- bin/docker/convert.sh
+++ bin/docker/convert.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp appizy-cli ./bin/appizy convert $@
+docker run -it --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp appizy/appizy-cli ./bin/appizy convert $@
